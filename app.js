61fd7397dc65b4446da3f2d9681bdf774208d75b
sant surat <santsurat123@gmail.com>
2022-08-02 17:33:31 +0530

    index.html
    new app

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 let btnTranslate = document.querySelector("#btn-translate");
 
-let txtInput = document.querySelector("#txt-input");
-console.log(txtInput)
+let InputTxt = document.querySelector("#Input-Txt");
+let outputDiv = document.querySelector("#output");
 
+// var serverURL = "http://127.0.0.1:5500/index.html?= I am sant surat"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
  function clickEventHandler() {
-console.log("clicked!");
-console.log("input", txtInput.value)
+let InputText = InputTxt.value;//Input
+
+fetch(getTranslationURL(InputText))//processing
+.then(response => response.json())
+.then(json => {
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText; // output
+   })
+.catch(errorHandler)
  }
 
 
-document.body.style.backgroundColor = "orange";
-btnTranslate.addEventListener("click", clickEventHandler )
+document.body.style.backgroundColor = "lightblue";
+btnTranslate.addEventListener("click", clickEventHandler );
+

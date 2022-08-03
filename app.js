2e5e8be21f37315ac94381d2ef9cb80874bd9652
sant surat <santsurat123@gmail.com>
2022-08-03 12:50:26 +0530

    index.html
    okay

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-let btnTranslate = document.querySelector("#btn-translate");
-
-let InputTxt = document.querySelector("#Input-Txt");
-let outputDiv = document.querySelector("#output");
-
-// var serverURL = "http://127.0.0.1:5500/index.html?= I am sant surat"
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL(text){
-    return serverURL + "?" + "text=" + text
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
 }
+
 function errorHandler(error) {
     console.log("error occured", error);
     alert("something wrong with server! try again after some time")
 }
- function clickEventHandler() {
-let InputText = InputTxt.value;//Input
-
-fetch(getTranslationURL(InputText))//processing
-.then(response => response.json())
-.then(json => {
-    var translatedText = json.contents.translated;
-    outputDiv.innerText = translatedText; // output
-   })
-.catch(errorHandler)
- }
 
 
-document.body.style.backgroundColor = "lightblue";
-btnTranslate.addEventListener("click", clickEventHandler );
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
 
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
